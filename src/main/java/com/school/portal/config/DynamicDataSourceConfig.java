0015a5e273abcbbe1dabb5c2b5e086122fe5b3e2
parasGyl30 <parasgoyal3012@gmail.com>
2025-07-01 16:31:07 +0530

    reverted db password change.

--- src/main/java/com/school/portal/config/DynamicDataSourceConfig.java
+++ src/main/java/com/school/portal/config/DynamicDataSourceConfig.java
@@ -30,7 +30,7 @@ public class DynamicDataSourceConfig {
         dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
         dataSourceBuilder.url("jdbc:mysql://localhost:3306/schoolportal?createDatabaseIfNotExist=true&useSSL=false");
         dataSourceBuilder.username("root");
-        dataSourceBuilder.password("root");
+        dataSourceBuilder.password("test");
         return dataSourceBuilder.build();
     }
     @Bean
@@ -39,7 +39,7 @@ public class DynamicDataSourceConfig {
         dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
         dataSourceBuilder.url("jdbc:mysql://localhost:3306/schoolportal?createDatabaseIfNotExist=true&useSSL=false");
         dataSourceBuilder.username("root");
-        dataSourceBuilder.password("root");
+        dataSourceBuilder.password("test");
         //d
         return dataSourceBuilder.build();
 
