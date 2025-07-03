89e96bd3a3a107d81e2902c35aad0f375fd3fcd6
AkashKumar <akashdodwal@gmail.com>
2025-07-03 15:36:13 +0530

    version and open all api bypass

--- src/main/java/com/school/portal/controller/FeeController.java
+++ src/main/java/com/school/portal/controller/FeeController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/fees")
+@RequestMapping("/api/v1/fees")
 public class FeeController {
 
     @Autowired
--- src/main/java/com/school/portal/controller/TeacherTimeTableController.java
+++ src/main/java/com/school/portal/controller/TeacherTimeTableController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/timetable")
+@RequestMapping("/api/v1/timetable")
 public class TeacherTimeTableController {
 
     @Autowired
