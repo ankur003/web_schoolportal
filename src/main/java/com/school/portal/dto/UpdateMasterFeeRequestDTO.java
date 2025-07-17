5580afc10a8fe342f5a5411f494a5a37e8ee96f7
AkashKumar <akashdodwal@gmail.com>
2025-07-17 21:30:26 +0530

    add masterfeeuuid and getter setter

--- src/main/java/com/school/portal/dto/UpdateMasterFeeRequestDTO.java
+++ src/main/java/com/school/portal/dto/UpdateMasterFeeRequestDTO.java
@@ -7,6 +7,8 @@ public class UpdateMasterFeeRequestDTO {
 	@NotNull
     private Double totalFee;
 
+	private String masterFeeUuid;
+
 	/**
 	 * @return the totalFee
 	 */
@@ -20,6 +22,13 @@ public class UpdateMasterFeeRequestDTO {
 	public void setTotalFee(Double totalFee) {
 		this.totalFee = totalFee;
 	}
-	
-	
+
+	public String getMasterFeeUuid() {
+		return masterFeeUuid;
+	}
+
+	public void setMasterFeeUuid(String masterFeeUuid) {
+		this.masterFeeUuid = masterFeeUuid;
+	}
+
 }
