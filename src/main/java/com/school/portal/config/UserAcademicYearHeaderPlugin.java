141a2dbca633bd13266dc97b29af5f52607b1614
Ankur <ankurbanssla@gmail.com>
2025-08-03 11:07:46 +0530

    add academic year changes

--- src/main/java/com/school/portal/config/UserAcademicYearHeaderPlugin.java
+++ src/main/java/com/school/portal/config/UserAcademicYearHeaderPlugin.java
@@ -1,28 +1,39 @@
 package com.school.portal.config;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
+
+import com.school.portal.enums.AcademicYear;
+
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.AllowableListValues;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.OperationBuilderPlugin;
 import springfox.documentation.spi.service.contexts.OperationContext;
-
-import java.util.Collections;
 
 @Component
 public class UserAcademicYearHeaderPlugin implements OperationBuilderPlugin {
 
     @Override
     public void apply(OperationContext context) {
-        // Add header only for GET methods
+        // Apply only to GET methods
         if ("GET".equalsIgnoreCase(context.httpMethod().name())) {
+            List<String> enumNames = Arrays.stream(AcademicYear.values())
+                    .map(Enum::name)
+                    .collect(Collectors.toList());
+
             context.operationBuilder().parameters(
                     Collections.singletonList(new ParameterBuilder()
                             .name("user_academic_year")
                             .description("Academic year of the user")
                             .modelRef(new ModelRef("string"))
                             .parameterType("header")
-                            .required(true) // set false if optional
+                            .required(true)
+                            .allowableValues(new AllowableListValues(enumNames, "string"))
                             .build())
             );
         }
